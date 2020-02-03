bedd3b5eb231c0f84e79e96165852cc2c0ab7156
Thow <tc676@cornell.edu>
2020-02-03 05:10:00 -0500

    Initial Commit

---
+++ FirebaseImages/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseImages
+//
+//  Created by Thow Min Cham on 2/3/20.
+//  Copyright © 2020 Thow Min Cham. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
